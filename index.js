63016dc62aa6b6baccf109762f3d0301db4e8757
Mehak Rathoure <146577616+Mehakrathoure@users.noreply.github.com>
2026-05-03 19:21:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
-import React from 'react';
-import delivery from './delivery.avif';
-import inactivedelivery from './inactive.webp';
-import dining from './diningout.jpg';
-import inactivedining from './inactivedining.jpg';
-import nightlife from './nightlife.jpg';
-import inactivenightlife from './inactivenightlife.webp';
-import './tabOptions.css';
+import React from "react";
+import "./collection.css";
+import NextArrow from "../carousel/nextArrow";
+import PrevArrow from "../carousel/prevArrow";
+import Slider from "react-slick";
 
-const tabs=[
-    {
-      id:1,
-      name:"Delivery",
-      active_img:delivery,
-      backdrop:"FCEEC0",
-      inactive_img: inactivedelivery,
-    },
-    {
-      id:2,
-      name:"Dining Out",
-      active_img:dining,
-      backdrop:"FCEEC0",
-      inactive_img: inactivedining,
-    },
-    {
-      id:3,
-      name:"Nightlife",
-      active_img:nightlife,
-      backdrop:"FCEEC0",
-      inactive_img: inactivenightlife,
-    },
-];
-
-const TabOptions = ({activeTab, setActiveTab})=> {
-    return (
-     <div className='tabOptions'>
-        <div className='max-width options-wrapper'>
-            {tabs.map((tab)=>{
-                return (
-                    <div onClick={()=>setActiveTab(tab.name)}
-                    className={`tab-item absolute-center cur-po ${activeTab===tab.name && "active-tab"}`}
-                    >
-                        <div className='tab-image-container absolute-center' style={{backgroundColor:`${activeTab===tab.name?tab.backdrop:""}`}}>
-                           <img className='tab-image' alt={tab.name} src={activeTab===tab.name?tab.active_img:tab.inactive_img}/>
-                        </div>
-                      <div className='tab-name'>
-                        {tab.name}
-                      </div>
-                    </div>
-                )
-            })}
-        </div> 
-     </div>
-    );
+const settings = {
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
 };
 
-export default TabOptions;
+const Collection = ({ list }) => {
+  return (
+    <div className="collection-wrapper">
+      <div className="max-width collection">
+        <div className="collection-title">Collections</div>
+        <div className="collection-subtitle-row">
+          <div className="collection-subtitle-text">
+            Explore curated lists of top restaurants, cafes, pubs, and bars in
+            Noida, based on trends
+          </div>
+          <div className="collection-location">
+            <div>All collections in Noida</div>
+          </div>
+        </div>
+        <Slider {...settings}>
+            {list.map((item)=>(
+                <div>
+                    <div className="collection-cover">
+                        <img src={item.cover} className="collection-image" alt={item.title}/>
+                        <div className="gradient-bg"></div>
+                        <div className="collection-card-title">{item.title}</div>
+                        <div className="collection-card-subtitle">
+                            <div>{item.places}</div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+export default Collection;
